5229fa025b41898e5231ac919493d43bd764202c
techdave <klatzdavie19@gmail.com>
2023-02-20 11:29:21 +0300

    initial setup set up node server and react frontend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// app
+const app = express();
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+app.use(cors(''));
+
+// routes
+app.get('/api', (req, res) => {
+  res.json({ time: Date().toString() });
+});
+
+// port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
